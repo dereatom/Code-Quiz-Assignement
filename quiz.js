786259824f6515a95262aacefa7c01d63ad2382e
Dereje Ayele <derejeatomsa2@gmail.com>
2021-03-22 21:01:37 -0400

    more work to the final

--- quiz.js
+++ quiz.js
@@ -1,12 +1,12 @@
 const question = document.getElementById("question");
 const choice = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choice)
+setTimeout(function(){ question.value = "1 seconds" }, 1000);
 
 let currentQuestion = {};
-let acceptingAnswers =false;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuestion = [];
 
 let questions = [
 {
@@ -26,12 +26,12 @@ let questions = [
     answer: 3,
 },
 {
-    question: "which one/s is/are Computer Programming Languages?",
+    question: "which are Computer Programming Languages?",
     choice1: "JavaScript",
     choice2: "HTML",
     choice3: "C++C#",
     choice4: "All",
-    Answer: 4,
+    Answer: 4
 }
 ]
 const CORRECT_BONUS =10;
@@ -39,48 +39,63 @@ const MAX_QUESTION = 3;
 startGame = () => {
     questionCounter= 0;
     score = 0;
-    availableQuestions= [...questions];
-    console.log(availableQuestions);
+    availableQuestion= [...questions];
+    
     getNewQuestion();
 
 };
 getNewQuestion = () => {
-    if(availableQuestions.length ===0 || questionCounter >= MAX_QUESTION)
-    return window.location.assign("/end.html")
+    if(availableQuestion.length ===0 || questionCounter >= MAX_QUESTION)
+    return window.location.assign("/Users/derej/workspaces/bootcamp/Homework/Code-Quiz-Assingment/Code-Quiz-Assignement/highscores.html")
     questionCounter++; 
-   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-   currentQuestion= availableQuestions[questionIndex];
-   questions.innerText = currentQuestion.question;
+   const questionIndex = Math.floor(Math.random() * availableQuestion.length);
+   currentQuestion= availableQuestion[questionIndex];
+   question.innerText = currentQuestion.question;
 //for each question we set as follow
  choice.forEach( choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
   });
-    availableQuestions.splice(questionIndex, 1);
+    availableQuestion.splice(questionIndex, 1);
 
    acceptingAnswers= true;
 };
 choice.forEach(choice =>{
     choice.addEventListener("click", e => {
+        
         if(!acceptingAnswers) 
         return;
         acceptingAnswers= false;
         const selectedChoice = e.target;
         const selectedAnsewer = selectedChoice.dataset["number"];
-        const classToApply= "incorrect";
-        if (selectedAnsewer === currentQuestion.answer){
-            classToApply = 'correct';
-        }
-        console.log(selectedAnsewer == currentQuestion.answer);
-        selectedChoice.parentElement.classList.add(classToApply)
-        setTimeOut( () =>{
-            selectedChoice.parentElement.classList.remove(classToApply)
+        
+       const classToApply = selectedAnsewer == currentQuestion.answer ? "correct": "incorrect";
+       
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
   
-            getNewQuestion();
+          getNewQuestion();
 
-        }, 1000);
+         }, 1000);
         
     });
 });
 startGame();
+var secondsLeft = 30;
 
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + "final score";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
